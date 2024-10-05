964369c96f3ad817b025d5a32f36d3ce610cb6a3
ershjora <153352626+ershjora@users.noreply.github.com>
2024-10-05 22:59:19 +0300

    lesson1

---
+++ src/lesson1/Task1.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        char g = 'g';
+        char e = 'e';
+        char o = 'o';
+        char r = 'r';
+        char i = 'i';
+
+       
+    }
+}
