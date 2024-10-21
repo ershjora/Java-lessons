a1bd333217a90ef8da822396b48b56a9d3e84481
ershjora <ershjora@yandex.ru>
2024-10-21 19:36:31 +0300

    Test

--- src/Jenya/prikolNomer8Wtoroi.java
+++ src/Jenya/prikolNomer8Wtoroi.java
@@ -8,14 +8,16 @@ public class prikolNomer8Wtoroi {
         System.out.println("Введите число: ");
         int Number = scanner.nextInt();
         int Degree = 0;
-        int Count =0;
+        int Answer =0;
         for (int i = 0; i < Number; ) {
             if (Number !=1 && Number %2 ==0){
-                Number/=2;
+                Answer= Number/=2;
                 Degree++;
+                if (Answer%2 !=0){
+                    System.out.println("Нет!");
+                }
             }
             else {
-                System.out.println("");
                 break;
             }
         }
